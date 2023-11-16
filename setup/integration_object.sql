d16b5c1bd8159af4724ab48f1cb47831b71318ca
chriswadebiz <97286932+chriswadebiz@users.noreply.github.com>
2023-11-16 09:15:46 +0000

    Update integration_object.sql
    Updated SQL code after AWS move to London.

--- setup/integration_object.sql
+++ setup/integration_object.sql
@@ -4,17 +4,17 @@ use database igg_source;
 use schema bloomberg_bond;
 use warehouse compute_wh;
 
-create or replace STORAGE INTEGRATION S3_INTERGRATION
+create or replace STORAGE INTEGRATION S3_INTEGRATION
 TYPE = EXTERNAL_STAGE
 STORAGE_PROVIDER = 'S3'
 ENABLED = TRUE
 STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::400693409844:role/snowflake'
-STORAGE_ALLOWED_LOCATIONS = ('s3://s3-biz-igg-dev-processed/23-10-27 Biztory Extended Dataset Example/');
+STORAGE_ALLOWED_LOCATIONS = ('s3://s3-biz-igg-lon-dev-processed/23-10-27 Biztory Extended Dataset Example/');
 
-desc integration S3_INTERGRATION;
+desc integration S3_INTEGRATION;
 -- arn + external id need to be added to snowflake role policy in AWS
 
 
 GRANT CREATE STAGE ON SCHEMA IGG_SOURCE_DEV.BLOOMBERG_BOND TO ROLE sysadmin;
 
-GRANT USAGE ON INTEGRATION S3_INTERGRATION TO ROLE sysadmin;
+GRANT USAGE ON INTEGRATION S3_INTEGRATION TO ROLE sysadmin;
