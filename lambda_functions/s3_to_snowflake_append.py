52a31e3b243b222de45273793f7984c66d8a8585
rklbiz <ruby.lalli@biztory.be>
2023-12-14 14:06:03 +0000

    changes

--- lambda_functions/s3_to_snowflake_append.py
+++ lambda_functions/s3_to_snowflake_append.py
@@ -1,18 +1,85 @@
-import boto3
-import io
-import datetime
 from openpyxl import load_workbook
-import pandas as pd
-import boto3
 import os
 from dotenv import load_dotenv
 import snowflake.connector
 from botocore.exceptions import NoCredentialsError, ClientError
 from snowflake.connector.errors import DatabaseError, ProgrammingError
-import urllib
+import logging
+
+# Configure logging
+logging.basicConfig(filename='script.log', level=logging.INFO)
 
 # Load environment variables from .env file
 load_dotenv()
+
+def find_values(file_name, data_mapping):
+    try:
+
+        for stage,table in data_mapping.items():
+            if os.path.dirname(file_name).split('/')[-1] in table:
+                for values in table.keys():
+                    FolderName = values
+                for files,formats in table.items():
+                    for vals,keys in formats.items():
+                        for names,file_format in keys.items():
+                            if names in os.path.basename(file_key).upper():
+                                # print(f"Schema_Name: {vals}")
+                                # print(f"File_Format_Name: '{file_format}'")
+                                # print(f"Table_Name: {names}")
+                                return vals,file_format,names,FolderName,stage
+            else:
+                logging.error(f"This schema does not exist in the current dictionary please add or check the name for file: {file_key}")
+            # print(f"Folder_Name: {FolderName}")
+            # print(f"Stage_Name: {stage}")
+                                
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+
+def connect_to_snowflake(user, password, account, role, warehouse, database, schema):
+    conn = snowflake.connector.connect(
+        user=user,
+        password=password,
+        account=account,
+        role=role,
+        warehouse=warehouse,
+        database=database,
+        schema=schema
+    )
+    return conn
+
+def snowflake_requirements(cur,WAREHOUSE,DATABASE,SCHEMA):
+    try:
+        cur.execute(f"USE WAREHOUSE {WAREHOUSE};")
+        cur.execute(f"USE {DATABASE}.{SCHEMA};")
+        print("Connected to Snowflake successfully.")
+    except Exception as e:
+        logging.error(f"An error occurred when trying to connect to snowflake. please check : {str(e)}")
+
+'''
+TODO: not sure if we need the stage and if we need to rerun it everytime when an updated files comes in but it is here for now
+'''
+# Upload file to Snowflake stage
+# query = f"CREATE OR REPLACE STAGE {snowflake_stage} \
+# STORAGE_INTEGRATION = S3_INTEGRATION \
+# URL = 's3://{os.environ['S3_BUCKET_NAME']}/{os.path.dirname(file_key)}' \
+# FILE_FORMAT = csv_format \
+# DIRECTORY = ( ENABLE = true AUTO_REFRESH = true );" 
+
+# cur.execute(query)
+
+# print("File uploaded to Snowflake stage successfully.")
+
+#Copy data into Snowflake table
+
+def copy_execute(snowflake_table,snowflake_stage,snowflake_file_format,cur):
+    try:
+        cur.execute(f"COPY INTO {snowflake_table} FROM '@{snowflake_stage}'\
+        FILE_FORMAT = csv_format \
+        PATTERN = '{snowflake_file_format}';")
+        print(f"Data copied into Snowflake table successfully for files {file_key}")
+    except Exception as e:
+        logging.error(f"There is an error in the copy_execute statement: {str(e)}")
+
 
 data_mapping = { 'STG_MARKET_CURVE': {
     'ICE_MarketCurve': {
@@ -27,44 +94,8 @@ data_mapping = { 'STG_MARKET_CURVE': {
     }
 }
 
-def find_values(file_name, data_mapping):
-    for stage,table in data_mapping.items():
-        if os.path.dirname(file_key).split('/')[-1] in table:
-            for values in table.keys():
-                FolderName = values
-            for files,formats in table.items():
-                for vals,keys in formats.items():
-                    for names,file_format in keys.items():
-                        if names in os.path.basename(file_key).upper():
-                            # print(f"Schema_Name: {vals}")
-                            # print(f"File_Format_Name: '{file_format}'")
-                            # print(f"Table_Name: {names}")
-        # print(f"Folder_Name: {FolderName}")
-        # print(f"Stage_Name: {stage}")
-                            return vals,file_format,names,FolderName,stage
-        
-
 file_key = "23-10-27 Biztory Extended Dataset Example/ICE_MarketCurve/DataX_FX_Spot_Forward_2023-12-04_1700LON_2023-12-04_170201LON.csv"
 
-def connect_to_snowflake(user, password, account, role, warehouse, database, schema):
-    conn = snowflake.connector.connect(
-        user=user,
-        password=password,
-        account=account,
-        role=role,
-        warehouse=warehouse,
-        database=database,
-        schema=schema
-    )
-    return conn
-
-# # # Specify the local file path where you want to save the downloaded file
-# # local_file_path = f"tmp/{file_key}"
-# # os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-
-# # s3_client = boto3.client('s3')
-# # s3_client.download_file(output_bucket,file_key,local_file_path)
-# # print("File downloaded from S3 successfully.")
 
 snowflake_schema = find_values(file_key, data_mapping)[0]
 snowflake_table = find_values(file_key, data_mapping)[2]
@@ -82,29 +113,11 @@ conn = connect_to_snowflake(user=os.environ.get("SNOWFLAKE_USER"),
 
 cur = conn.cursor()
 
-cur.execute(f"USE WAREHOUSE {os.environ['SNOWFLAKE_WAREHOUSE']};")
+snowflake_requirements(cur=cur,WAREHOUSE=os.environ.get("SNOWFLAKE_WAREHOUSE"),DATABASE=os.environ.get("SNOWFLAKE_DATABASE"),SCHEMA=os.environ.get("SNOWFLAKE_SCHEMA"))
 
-cur.execute(f"USE {os.environ['SNOWFLAKE_DATABASE']}.{os.environ['SNOWFLAKE_SCHEMA']};")
-print("Connected to Snowflake successfully.")
+copy_execute(snowflake_table=snowflake_table,snowflake_stage=snowflake_stage,snowflake_file_format=snowflake_file_format,cur=cur    )
 
-# Upload file to Snowflake stage
-query = f"CREATE OR REPLACE STAGE {snowflake_stage} \
-STORAGE_INTEGRATION = S3_INTEGRATION \
-URL = 's3://{os.environ['S3_BUCKET_NAME']}/{os.path.dirname(file_key)}' \
-FILE_FORMAT = csv_format \
-DIRECTORY = ( ENABLE = true AUTO_REFRESH = true );" 
 
-cur.execute(query)
-
-print("File uploaded to Snowflake stage successfully.")
-
-#Copy data into Snowflake table
-
-cur.execute(f"COPY INTO {snowflake_table} FROM '@{snowflake_stage}'\
-FILE_FORMAT = csv_format \
-PATTERN = '{snowflake_file_format}';")
-
-print(f"Data copied into Snowflake table successfully for files {file_key}")
 
 '''
 TODO: Function to map schema/tables to filenames in dictionary
